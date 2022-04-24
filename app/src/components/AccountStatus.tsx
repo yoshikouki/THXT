46006a9eddb3415bae874fbf34fbaac2fda1aac7
yoshikouki <yoshikouki@gmail.com>
2022-04-25 00:03:55 +0900

    Wallet 接続まで

--- app/src/components/AccountStatus.tsx
+++ app/src/components/AccountStatus.tsx
@@ -1,10 +1,19 @@
 import { useCallback, useState } from "react";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 
 const AccountStatus = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const connectWallet = useCallback(() => {
+  const [account, setAccount] = useState<string | null>(null);
+  const connectWallet = useCallback(async () => {
     try {
       setIsLoading(true);
+      const web3Modal = new Web3Modal();
+      const instance = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(instance);
+      const accounts = await provider.send("eth_accounts", []);
+      const fetchedAccount = accounts[0];
+      setAccount(fetchedAccount);
     } catch (e) {
       console.error(e);
     } finally {
@@ -14,8 +23,10 @@ const AccountStatus = () => {
 
   return isLoading ? (
     <button className="btn btn-ghost loading"></button>
+  ) : account ? (
+    <div className="font-bold">{account}</div>
   ) : (
-    <button className="btn btn-ghost" onClick={connectWallet}>
+    <button className="btn btn-ghost" onClick={() => void connectWallet()}>
       Connect Wallet
     </button>
   );
