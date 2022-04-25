cc1b5687b1faba8071d2703ab2bf389cbe2f8852
yoshikouki <yoshikouki@gmail.com>
2022-04-25 13:57:26 +0900

    Disconnect Wallet

--- app/src/components/AccountStatus.tsx
+++ app/src/components/AccountStatus.tsx
@@ -5,15 +5,20 @@ import { ethers } from "ethers";
 const AccountStatus = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [account, setAccount] = useState<string | null>(null);
+  const disconnectWallet = useCallback(() => {
+    setAccount(null);
+  }, []);
   const connectWallet = useCallback(async () => {
     try {
       setIsLoading(true);
-      const web3Modal = new Web3Modal();
+      const web3Modal = new Web3Modal({
+        cacheProvider: true,
+      });
       const instance = await web3Modal.connect();
       const provider = new ethers.providers.Web3Provider(instance);
+      provider.on("disconnect", disconnectWallet);
       const accounts = await provider.send("eth_accounts", []);
-      const fetchedAccount = accounts[0];
-      setAccount(fetchedAccount);
+      setAccount(accounts[0]);
     } catch (e) {
       console.error(e);
     } finally {
@@ -24,7 +29,19 @@ const AccountStatus = () => {
   return isLoading ? (
     <button className="btn btn-ghost loading"></button>
   ) : account ? (
-    <div className="font-bold">{account}</div>
+    <div className="dropdown dropdown-hover">
+      <label className="btn btn-ghost">{account}</label>
+      <ul
+        className="p-4 shadow dropdown-content menu bg-base-100 rounded-box"
+        tabIndex={0}
+      >
+        <li>
+          <label className="btn btn-ghost" onClick={disconnectWallet}>
+            Disconnect
+          </label>
+        </li>
+      </ul>
+    </div>
   ) : (
     <button className="btn btn-ghost" onClick={() => void connectWallet()}>
       Connect Wallet
